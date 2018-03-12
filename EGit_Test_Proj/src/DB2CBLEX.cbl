ea12635d67a3490c1313e5a11929e30553f23b40
llock <llock@ADMINIB-7OR5U6F.mkm.can.ibm.com>
2018-03-12 12:03:30 -0400

    Updated order by LASTNAME

--- EGit_Test_Proj/src/DB2CBLEX.cbl
+++ EGit_Test_Proj/src/DB2CBLEX.cbl
@@ -58,6 +58,7 @@
       *  LOUIS - ADDED A REPORT TITLE                 2017-11-14       *00540733
       *  LOUIS - ADDED ORDER BY TO DB2 CURSOR         2018-02-16       *00540733
       *  LOUIS - CHANGED ORDER BY SALARY              2018-03-05       *00540733
+      *  LOUIS - CHANGED ORDER BY LASTNAME            2018-03-12       *00540733
       *                                                                *00540826
       ******************************************************************00550026
       ***           E N D  P R O G R A M  C H A N G E  L O G           *00560026
@@ -138,7 +139,7 @@
                             WORKDEPT,                                   02130026
                             SALARY                                      02140026
                      FROM DSN8110.EMP                                   02150026
-                     ORDER BY SALARY ASC                                02160026
+                     ORDER BY LASTNAME ASC                              02160026
                                                                         02160026
                    END-EXEC.                                            02170026
                                                                         02180026
